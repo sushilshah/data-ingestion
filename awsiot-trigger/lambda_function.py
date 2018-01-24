3a8264cf2210b3d68a27a97210239510f6845827
Sushil Kumar Shah <sushil.shah@incedoinc.com>
2018-01-24 22:47:56 +0530

    working trigger

--- awsiot-trigger/lambda_function.py
+++ awsiot-trigger/lambda_function.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import datetime
 import time
 from random import randint
 import random
@@ -13,13 +12,21 @@ def lambda_handler(event, context):
     '''
     Insert alert data into DynamoDB
     '''
-    table_name = event['table_name'] if 'table_name' in event else 'device_status'
-    payload = create_mock_data(event)
-    alerts = check_notify_alert(payload)
-    payload['alerts'] = alerts
-    payload['updated'] = int(time.time())
-    response = DynamoDB().insert_item(table_name, payload)
-    return response
+    try:
+        logger.info("Processing event ::")
+        logger.info(event)
+
+        table_name = event['table_name'] if 'table_name' in event else 'device_status'
+        payload = create_mock_data(event)
+        alerts = check_notify_alert(payload)
+        payload['alerts'] = alerts
+        payload['updated'] = int(time.time())
+        logger.info("payload for dynamodb insert::")
+        logger.info(payload)
+        response = DynamoDB().insert_item(table_name, payload)
+        return response
+    except Exception as exp:
+        logger.exception("Exception occured in lambda func %s", exp)
 
 def create_mock_data(base_obj):
     '''
@@ -28,23 +35,26 @@ def create_mock_data(base_obj):
     :param base_obj (object):  {"id" : "xxxxxxUUIDxxxxxx", "name" : "device_name"}
     :returns: The mock data dict
     '''
-    mock = dict()
-    if not base_obj or not 'id' in base_obj:
-        logger.info("Id not present in base obj, create pick random devices from mock")
-        mock_devices = {'Sensor-300578' : '111',
-                        'Sensor-300577' : '222',
-                        'Sensor-300575' : '333',
-                        'Sensor-300579' : '444',
-                        'Sensor-195732' : '555'}
-        device_name, device_id = random.choice(list(mock_devices.items()))
-        mock['id'] = device_id
-        mock['name'] = device_name
+    try:
+        mock = dict()
+        if not base_obj or not 'id' in base_obj:
+            logger.info("Id not present in base obj, create pick random devices from mock")
+            mock_devices = {'Sensor-300578' : '111',
+                            'Sensor-300577' : '222',
+                            'Sensor-300575' : '333',
+                            'Sensor-300579' : '444',
+                            'Sensor-195732' : '555'}
+            device_name, device_id = random.choice(list(mock_devices.items()))
+            mock['id'] = device_id
+            mock['name'] = device_name
 
-    '''Acceptable values:
-    x_mms : 0-18, y_mms : 0-21, z_mms : 0-15, x_hz  : 5-47, y_hz  : 5-31, z_hz  : 5-51'''
-    mock['x_mms'], mock['y_mms'], mock['z_mms'], mock['x_hz'], mock['y_hz'], mock['z_hz'] = \
-    [randint(0,25), randint(0,30), randint(0,25), randint(5,60), randint(5,50), randint(5,60)]
-    return mock
+        '''Acceptable values:
+        x_mms : 0-18, y_mms : 0-21, z_mms : 0-15, x_hz  : 5-47, y_hz  : 5-31, z_hz  : 5-51'''
+        mock['x_mms'], mock['y_mms'], mock['z_mms'], mock['x_hz'], mock['y_hz'], mock['z_hz'] = \
+        [randint(0,25), randint(0,30), randint(0,25), randint(5,60), randint(5,50), randint(5,60)]
+        return mock
+    except Exception as exp:
+        raise exp
 
 def check_notify_alert(d_reading_obj):
     '''
@@ -57,17 +67,26 @@ def check_notify_alert(d_reading_obj):
     :param d_reading_obj (object): Sensor readings
     :returns: Dict of Alerts
     '''
-    alerts = {}
-    if not (0 <= d_reading_obj['x_mms'] <= 18):
-        alerts['x_mms'] = d_reading_obj['x_mms']
-    if not (0 <= d_reading_obj['y_mms'] <= 21):
-        alerts['y_mms'] = d_reading_obj['y_mms']
-    if not (0 <= d_reading_obj['z_mms'] <= 15):
-        alerts['z_mms'] = d_reading_obj['z_mms']
-    if not (5 <= d_reading_obj['x_hz'] <= 47):
-        alerts['x_hz'] = d_reading_obj['x_hz']
-    if not (5 <= d_reading_obj['y_hz'] <= 31):
-        alerts['y_hz'] = d_reading_obj['y_hz']
-    if not (5 <= d_reading_obj['z_hz'] <= 31):
-        alerts['z_hz'] = d_reading_obj['z_hz']
-    return alerts    
+    try:
+        alerts = {}
+        if not (0 <= d_reading_obj['x_mms'] <= 18):
+            alerts['x_mms'] = d_reading_obj['x_mms']
+        if not (0 <= d_reading_obj['y_mms'] <= 21):
+            alerts['y_mms'] = d_reading_obj['y_mms']
+        if not (0 <= d_reading_obj['z_mms'] <= 15):
+            alerts['z_mms'] = d_reading_obj['z_mms']
+        if not (5 <= d_reading_obj['x_hz'] <= 47):
+            alerts['x_hz'] = d_reading_obj['x_hz']
+        if not (5 <= d_reading_obj['y_hz'] <= 31):
+            alerts['y_hz'] = d_reading_obj['y_hz']
+        if not (5 <= d_reading_obj['z_hz'] <= 31):
+            alerts['z_hz'] = d_reading_obj['z_hz']
+        return alerts    
+    except Exception as exp:
+        raise exp
+
+for i in range(100):
+    event = {}
+    payload = create_mock_data(event)
+    alerts = check_notify_alert(payload)
+    print(alerts, i)
